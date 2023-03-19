e91c3ae98c98f9dc42b105548a10c235985c80b4
Smart7 <wenxinming2684@gmail.com>
2023-03-19 16:53:59 +0800

    implement acapp acwing auth2 login

---
+++ game/views/settings/acwing/acapp/apply_code.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+from urllib.parse import quote
+from random import randint
+from django.core.cache import cache
+
+def get_state():
+    res = ""
+    for i in range(8):
+        res += str(randint(0, 9))
+    return res
+
+
+def apply_code(request):
+    appid = "4905"
+    redirect_uri = quote("https://app4905.acapp.acwing.com.cn/settings/acwing/acapp/receive_code/")
+    scope = "userinfo"
+    state = get_state()
+
+    cache.set(state, True, 7200)
+
+    return JsonResponse({
+        'result': "success",
+        'appid': appid,
+        'redirect_uri': redirect_uri,
+        'scope': scope,
+        'state': state,
+    })
